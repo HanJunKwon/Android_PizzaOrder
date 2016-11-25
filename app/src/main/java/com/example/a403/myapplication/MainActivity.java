85db76ca0f67bad6d91722191852f66f0cb40a24
권한준 <skclaqks11@naver.com>
2016-11-25 20:07:57 +0900

    기능 구현완료(결제 반복 선택 시 총 결제금액이 누적됨 수정할것)

--- app/src/main/java/com/example/a403/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a403/myapplication/MainActivity.java
@@ -1,13 +1,85 @@
 package com.example.a403.myapplication;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    private double totalAmount=0;
+    EditText et1,et2,et3;
+    CheckBox cb1;
+    RadioGroup rg1;
+    ImageView iv1;
+    Button bt1;
+    TextView tv1,tv2,tv3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setEdit();
+        setCheckBox();
+        setRadioGroup();
+        setImageView();
+        setButton();
+        setTextView();
+    }
+    void setEdit(){
+        et1 = (EditText)findViewById(R.id.editText1);
+        et2 = (EditText)findViewById(R.id.editText2);
+        et3 = (EditText)findViewById(R.id.editText3);
+    }
+    void setCheckBox(){
+        cb1 = (CheckBox)findViewById(R.id.checkBox);
+    }
+    void setRadioGroup(){
+        rg1 = (RadioGroup)findViewById(R.id.radioGroup);
+        rg1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId==R.id.radioButton)
+                    iv1.setImageResource(R.drawable.pickle);
+                else if(checkedId==R.id.radioButton2)
+                    iv1.setImageResource(R.drawable.hotsource);
+            }
+        });
+    }
+    void setImageView(){
+        iv1 = (ImageView)findViewById(R.id.imageView);
+    }
+    void setButton(){
+        bt1 = (Button)findViewById(R.id.button);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pizzaCount = Integer.parseInt(et1.getText().toString());
+                int spagettiCount = Integer.parseInt(et2.getText().toString());
+                int saladCount = Integer.parseInt(et3.getText().toString());
+                totalAmount += 16000*pizzaCount;
+                totalAmount += 11000*spagettiCount;
+                totalAmount += 4000*saladCount;
+                if(cb1.isChecked())
+                    totalAmount -=totalAmount*(7/100);
+                tv1.setText("주문개수 : "+(pizzaCount+spagettiCount+saladCount));
+                String Amount= String.format("%.1f", totalAmount);
+                tv2.setText("주문금액 : "+Amount);
+            }
+        });
+    }
+
+    void setTextView(){
+        tv1 = (TextView)findViewById(R.id.textView1);
+        tv2 = (TextView)findViewById(R.id.textView2);
+        tv3 = (TextView)findViewById(R.id.textView3);
     }
 }
