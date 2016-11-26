13d3669f54332163c8505c9892cafa1e088ec04e
권한준 <skclaqks11@naver.com>
2016-11-27 01:26:00 +0900

    라디오 이벤트 추가+체크박스 할인 적용+결제버튼 클릭시 결제 비용 누적 오류 수정

--- app/src/main/java/com/example/a403/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a403/myapplication/MainActivity.java
@@ -16,6 +16,9 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private double totalAmount=0;
+    final int pizza=16000;
+    final int spagetti=11000;
+    final int salad = 4000;
     EditText et1,et2,et3;
     CheckBox cb1;
     RadioGroup rg1;
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("khw");
         setEdit();
         setCheckBox();
         setRadioGroup();
@@ -42,15 +46,20 @@ public class MainActivity extends AppCompatActivity {
     void setCheckBox(){
         cb1 = (CheckBox)findViewById(R.id.checkBox);
     }
+
     void setRadioGroup(){
         rg1 = (RadioGroup)findViewById(R.id.radioGroup);
         rg1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if(checkedId==R.id.radioButton)
+                if(checkedId==R.id.radioButton) {
                     iv1.setImageResource(R.drawable.pickle);
-                else if(checkedId==R.id.radioButton2)
+                    tv3.setText("피클을 선택하셨습니다");
+                }
+                else if(checkedId==R.id.radioButton2) {
                     iv1.setImageResource(R.drawable.hotsource);
+                    tv3.setText("소스를 선택하셨습니다");
+                }
             }
         });
     }
@@ -65,14 +74,15 @@ public class MainActivity extends AppCompatActivity {
                 int pizzaCount = Integer.parseInt(et1.getText().toString());
                 int spagettiCount = Integer.parseInt(et2.getText().toString());
                 int saladCount = Integer.parseInt(et3.getText().toString());
-                totalAmount += 16000*pizzaCount;
-                totalAmount += 11000*spagettiCount;
-                totalAmount += 4000*saladCount;
+                totalAmount += pizza*pizzaCount;
+                totalAmount += spagetti*spagettiCount;
+                totalAmount += salad*saladCount;
                 if(cb1.isChecked())
-                    totalAmount -=totalAmount*(7/100);
+                    totalAmount = totalAmount*0.93;
                 tv1.setText("주문개수 : "+(pizzaCount+spagettiCount+saladCount));
                 String Amount= String.format("%.1f", totalAmount);
                 tv2.setText("주문금액 : "+Amount);
+                totalAmount = 0;
             }
         });
     }
@@ -81,5 +91,6 @@ public class MainActivity extends AppCompatActivity {
         tv1 = (TextView)findViewById(R.id.textView1);
         tv2 = (TextView)findViewById(R.id.textView2);
         tv3 = (TextView)findViewById(R.id.textView3);
+        tv3.setText("피클을 선택하셨습니다");
     }
 }
